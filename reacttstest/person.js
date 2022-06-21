d57263b3685328d7764fc485d1f397071d578c0a
Hojin Kim <zxcv05999@naver.com>
2022-06-21 10:44:55 +0900

    Typescript Polymorphism -ing

--- reacttstest/person.js
+++ reacttstest/person.js
@@ -6,9 +6,25 @@ class Person {
         this.name = name;
     }
     sayHello() {
-        return 'Hello' + this.name;
+        return "Hello" + this.name;
     }
 }
 exports.Person = Person;
-const persons = new Person('Lee');
+const persons = new Person("Lee");
 console.log(persons.sayHello());
+const pl = ["daniel", 1];
+const addSome = (a, b) => a + b;
+const push = (config) => {
+    if (typeof config === "string")
+        console.log(config);
+    else {
+        console.log(config.path, config.state);
+    }
+};
+const add = (a, b, c) => {
+    if (c)
+        return a + b + c;
+    return a + b;
+};
+add(1, 2);
+add(1, 2, 3); // 둘 다 가능해진다!
--- reacttstest/person.ts
+++ reacttstest/person.ts
@@ -1,15 +1,51 @@
-export class Person{
-    protected name : string;
+export class Person {
+  protected name: string;
 
-    constructor(name : string){
-        this.name = name;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 
-    sayHello(){
-        return 'Hello' + this.name;
-    }
+  sayHello() {
+    return "Hello" + this.name;
+  }
 }
 
-const persons = new Person('Lee');
+const persons = new Person("Lee");
 
 console.log(persons.sayHello());
+
+const pl: readonly [string, number] = ["daniel", 1];
+
+type Add = (a: number, b: number) => number;
+
+const addSome: Add = (a, b) => a + b;
+
+type Config = {
+  path: string;
+  state: object;
+};
+
+type Push = {
+  (path: string): void;
+  (confing: Config): void;
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") console.log(config);
+  else {
+    console.log(config.path, config.state);
+  }
+};
+
+type Ads = {
+  (a: number, b: number, c: number): number;
+  (a: number, b: number): number;
+};
+
+const add: Ads = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+add(1, 2);
+add(1, 2, 3); // 둘 다 가능해진다!
